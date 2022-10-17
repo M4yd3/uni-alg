9f8f7c3a01644f242f0191566db24f908ce6d70d
m4yd3 <marchenko151003@mail.ru>
2022-10-17 17:36:02 +0300

    Finish lab4

--- src/mirea/ru/marchenko/kvbo0121/lab4/Algorithms.java
+++ src/mirea/ru/marchenko/kvbo0121/lab4/Algorithms.java
@@ -1,27 +1,25 @@
 package mirea.ru.marchenko.kvbo0121.lab4;
 
-import java.util.LinkedList;
-
 public class Algorithms {
     private final static boolean RIGHT_TO_LEFT = false;
-    private final static boolean LEFT_TO_RIGHT = true;
 
-    static LinkedList<int[]> permutations(int n) {
-        final var permutations = new LinkedList<int[]>();
+    static int[][] permutations(int n) {
+        int permutationsLength = factorial(n);
 
-        int[] currentPermutation = new int[n];
-
+        int[] permutation = new int[n];
         boolean[] directions = new boolean[n];
-
         for (int i = 0; i < n; i++) {
-            currentPermutation[i] = i + 1;
+            permutation[i] = i + 1;
+            directions[i] = RIGHT_TO_LEFT;
         }
-        permutations.add(currentPermutation);
 
-        for (int i = 0; i < n; i++) directions[i] = RIGHT_TO_LEFT;
+        final var permutations = new int[permutationsLength][n];
+        permutations[0] = permutation.clone();
 
-        for (int i = 1; i < factorial(n); i++) {
-            currentPermutation = generatePermutation(currentPermutation, directions[i], n);
+
+        for (int i = 1; i < permutationsLength; i++) {
+            generateNextPermutation(permutation, directions);
+            permutations[i] = permutation.clone();
         }
 
         return permutations;
@@ -29,11 +27,52 @@ public class Algorithms {
 
     private static int factorial(int n) {
         int ans = 1;
-        for (int i = 0; i <= n; i++) ans *= i;
+        for (int i = 1; i <= n; i++) ans *= i;
         return ans;
     }
 
-    private static int[] generatePermutation(int[] previous, boolean direction, int n) {
-        return new int[n];
+    private static void generateNextPermutation(int[] permutation, boolean[] directions) {
+        int[] m = findMobileValueAndIndex(permutation, directions);
+        int mobileNumber = m[0];
+        int mobileIndex = m[1];
+
+        boolean mobileGoesLeft = directions[permutation[mobileIndex] - 1] == RIGHT_TO_LEFT;
+        int temp = permutation[mobileIndex];
+
+        if (mobileGoesLeft) {
+            permutation[mobileIndex] = permutation[mobileIndex - 1];
+            permutation[mobileIndex - 1] = temp;
+        } else {
+            permutation[mobileIndex] = permutation[mobileIndex + 1];
+            permutation[mobileIndex + 1] = temp;
+        }
+
+        for (int i : permutation) if (i > mobileNumber) directions[i - 1] = !directions[i - 1];
+
     }
+
+    private static int[] findMobileValueAndIndex(int[] permutation, boolean[] directions) {
+        final int n = permutation.length;
+        int mobile = 0, index = 0;
+
+        for (int i = 0; i < n; i++) {
+            int number = permutation[i];
+
+            boolean goesLeft = directions[number - 1] == RIGHT_TO_LEFT;
+            boolean canGoLeft = i != 0 && goesLeft && number > permutation[i - 1];
+            boolean canGoRight = i != n - 1 && !goesLeft && number > permutation[i + 1];
+            boolean isMobile = canGoLeft || canGoRight;
+
+            boolean largerThanPrevious = number > mobile;
+
+            if (largerThanPrevious && isMobile) {
+                mobile = number;
+                index = i;
+            }
+        }
+
+        return new int[]{mobile, index};
+    }
+
+
 }
--- src/mirea/ru/marchenko/kvbo0121/lab4/Main.java
+++ src/mirea/ru/marchenko/kvbo0121/lab4/Main.java
@@ -1,7 +1,25 @@
 package mirea.ru.marchenko.kvbo0121.lab4;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final String[] colors = {
+            "\033[0;32m", "\033[0;34m", "\033[0;33m", "\033[0;31m", "\033[0;36m", "\033[0m"
+    };
+
     // Steinhaus–Johnson–Trotter algorithm
     public static void main(String[] args) {
+        final Scanner in = new Scanner(System.in);
+        final int n = in.nextInt();
+
+        final int[][] permutations = Algorithms.permutations(n);
+
+        for (final int[] permutation : permutations) {
+            for (final int number : permutation) {
+                System.out.print(colors[(number - 1) % colors.length]);
+                System.out.print(number);
+            }
+            System.out.println();
+        }
     }
 }
