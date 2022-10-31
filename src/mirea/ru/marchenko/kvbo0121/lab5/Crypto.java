b1acb0e69a69ee9cd93490e9d33a6470942feaef
m4yd3 <marchenko151003@mail.ru>
2022-10-31 18:14:54 +0300

    Finish lab5

--- src/mirea/ru/marchenko/kvbo0121/lab5/Crypto.java
+++ src/mirea/ru/marchenko/kvbo0121/lab5/Crypto.java
@@ -5,31 +5,86 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Crypto {
-    // Shift direction - right
-    private final int shiftSize = 2; // bits
-    private final int blockSize = 2; // byte
-    private final Random generator;
+    private final boolean shiftDirection = true; // left = 0, right = 1
+    private final int shiftSize = 2;
+    private final int blockSize = 4;
+    private final long seed;
 
     public Crypto(long seed) {
-        generator = new Random(seed);
+        this.seed = seed;
     }
 
     public byte[] encode(String message) {
-        byte[] bytes = message.getBytes();
+        final byte[] bytes = message.getBytes();
+        final byte[] encodedBytes = new byte[Math.ceilDiv(bytes.length, blockSize) * blockSize];
+        final Random generator = new Random(seed);
 
         for (int i = 0; i < bytes.length; i += blockSize) {
-            final byte[] block = Arrays.copyOfRange(bytes, i, Math.min(i + blockSize, bytes.length));
-            System.out.println(Integer.toBinaryString(block[0]));
-            System.out.println(Integer.toBinaryString(block[1]));
-            System.out.println(Integer.toBinaryString((block[0] << 8) | block[1]));
-            break;
+            // Put bytes into block
+            int block = 0;
+            for (int j = i; j < Math.min(i + blockSize, bytes.length); j++) {
+                block = block << 8 | bytes[j];
+            }
+
+            // XOR cipher
+            final int gamma = Math.abs(generator.nextInt());
+            block ^= gamma;
+
+            // Shifting
+            if (shiftDirection) {
+                block = Integer.rotateRight(block, shiftSize);
+            } else {
+                block = Integer.rotateLeft(block, shiftSize);
+            }
+
+            for (int j = i; j < i + blockSize; j++) {
+                encodedBytes[j] = (byte) (block >>> 24);
+                block = block << 8;
+            }
         }
 
-        return bytes;
+        return encodedBytes;
+    }
+
+    private void printAsBinary(int x) {
+        String s = Integer.toBinaryString(x);
+        String f = "%" + (int) Math.ceil(s.length() / 8.0) * 8 + "s";
+        s = String.format(f, s).replaceAll(" ", "0");
+        System.out.printf("%32s%n", s);
     }
 
     public String decode(byte[] bytes) {
+        Random generator = new Random(seed);
+        byte[] decodedBytes = new byte[bytes.length];
+        int byteLength = 0;
 
-        return "";
+        for (int i = 0; i < bytes.length; i += blockSize) {
+            // Put bytes into block
+            int block = 0;
+            for (int j = i; j < Math.min(i + blockSize, bytes.length); j++) {
+                block = block << 8 | (bytes[j] & 255);
+            }
+
+            // Shifting back
+            if (!shiftDirection) {
+                block = Integer.rotateRight(block, shiftSize);
+            } else {
+                block = Integer.rotateLeft(block, shiftSize);
+            }
+
+            // XOR cipher
+            final int gamma = Math.abs(generator.nextInt());
+            block ^= gamma;
+
+            int blockLength = 4 - Integer.numberOfLeadingZeros(block) / 8;
+            block = Integer.reverseBytes(block);
+            for (int j = i + blockLength - 1; j >= i; j--) {
+                decodedBytes[j] = (byte) (block >>> 24);
+                block = block << 8;
+                byteLength++;
+            }
+        }
+        decodedBytes = Arrays.copyOfRange(decodedBytes, 0, byteLength);
+        return new String(decodedBytes);
     }
 }
--- src/mirea/ru/marchenko/kvbo0121/lab5/Main.java
+++ src/mirea/ru/marchenko/kvbo0121/lab5/Main.java
@@ -1,6 +1,6 @@
 package mirea.ru.marchenko.kvbo0121.lab5;
 
-import java.util.Random;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,11 +8,13 @@ public class Main {
         final long seed = 678499869;
         final Crypto crypto = new Crypto(seed);
 
-//        final Scanner in = new Scanner(System.in);
-//        final String message = in.nextLine();
-        final String message = "Hello, World!";
+        final Scanner in = new Scanner(System.in);
+        final String message = in.nextLine();
 
         final byte[] encodedMessage = crypto.encode(message);
+        System.out.println(Arrays.toString(encodedMessage));
+        
         final String decodedMessage = crypto.decode(encodedMessage);
+        System.out.println(decodedMessage);
     }
 }
